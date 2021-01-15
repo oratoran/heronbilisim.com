c2e6422111d92f29cbee9a19b235b184a5235fb6
Ozgur Dincer <ozgurdincer@kentajans.com>
2021-01-15 23:50:11 +0300

    minor updates

--- src/App.svelte
+++ src/App.svelte
@@ -8,9 +8,9 @@
 	<p>
 		<a href="mailto:info@heronbilisim.com">info@heronbilisim.com</a>
 	</p>
-	<figure class="image">
-		<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6022.835576229831!2d29.10244!3d40.994228!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2ecc10b2dfa999dd!2sHeron%20Bili%C5%9Fim!5e0!3m2!1str!2str!4v1590873491453!5m2!1str!2str" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-	</figure>
+	
+	<iframe title="google-map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12045.671152459661!2d29.1024398!3d40.994228!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2ecc10b2dfa999dd!2sHeron%20Bili%C5%9Fim!5e0!3m2!1sen!2str!4v1610731002597!5m2!1sen!2str" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+	
 	<h3 class="subtitle">Heron Bilişim</h3>
 	<p>Deluxia Suites A Blok 16
 		<br>Barbaros Mahallesi Mor Menekşe Sokak
